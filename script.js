ca1dee85a4dde59bb544311d206808b55461082b
prashantbhoite123 <pbhoite985@gmail.com>
2024-04-28 21:50:13 +0530

    minner change

--- script.js
+++ script.js
@@ -2,50 +2,48 @@ const userVal = document.querySelector(".userVal");
 const generteCol = document.querySelector("button");
 const allinput = document.querySelectorAll("input");
 const copyBtn = document.querySelector(".copyBtn");
-console.log(copyBtn);
-
 const newInputs = Array.from(allinput);
 
 function checkval(inval) {
   generteCol.addEventListener("click", (e) => {
     e.target.innerHTML = "Change Password";
-    const sym = `a%bc!d!e*fh%i%$j%k*lm*n$#`;
-    const str = `abcdefghijklmnopqrstuvwxyz`;
-    const upper = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    const sym = `a%bc!d!e*fh%34i%34$j%3434k*lm*n$#`;
+    const str = `abcdefg563hij3kl45mno35pqrstuvwxyz`;
     let lowerString = "";
+    
     let SymbolString = "";
-    let capital = "";
+
     for (let i = 0; i < 8; i++) {
       const lowerran = Math.floor(Math.random() * 8 + 1);
       const symbol = Math.floor(Math.random() * 8 + 1);
-      const upperVal = Math.floor(Math.random() * 8 + 1);
-      capital += upper[upperVal];
       lowerString += str[lowerran];
       SymbolString += sym[symbol];
     }
+    let capital = lowerString.toUpperCase();
     const speech = new SpeechSynthesisUtterance();
-    speech.text = "Paasword Generated";
+    speech.text = "number password is generated";
     window.speechSynthesis.speak(speech);
     if (inval.target.id === "numberChe") {
+      speech.text = " Number Paasword Generated";
       userVal.innerHTML = `<div>${Math.floor(
         Math.random() * 99999999 + 10000000
       )}</div>`;
       copyBtn.innerHTML = ` <i class="fa-regular fa-copy"></i>`;
-      speech.text = " Number Paasword Generated";
     } else if (inval.target.id === "lowerChe") {
+      speech.text = " Lowercase Paasword Generated";
       userVal.innerHTML = `<div>${lowerString}</div>`;
       copyBtn.innerHTML = ` <i class="fa-regular fa-copy"></i>`;
     } else if (inval.target.id === "UpperChe") {
+      speech.text = " Uppercase Paasword Generated";
       userVal.innerHTML = `<div>${capital}</div>`;
       copyBtn.innerHTML = ` <i class="fa-regular fa-copy"></i>`;
     } else if (inval.target.id === "SymbolChe") {
+      speech.text = " Symbol Paasword Generated";
       userVal.innerHTML = `<div>   ${SymbolString}  </div>`;
       copyBtn.innerHTML = ` <i class="fa-regular fa-copy"></i>`;
     }
   });
 }
-
-let foundcheked = false;
 newInputs.forEach((input) => {
   input.addEventListener("input", (e) => {
     checkval(e);
@@ -54,6 +52,9 @@ newInputs.forEach((input) => {
 
 copyBtn.addEventListener("click", (e) => {
   console.log("button was click");
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = " your password is copyed";
+  window.speechSynthesis.speak(speech);
   copyBtn.innerHTML === `<i class="fa-solid fa-copy"></i>`
     ? (copyBtn.innerHTML = `<i class="fa-regular fa-copy"></i>`)
     : (copyBtn.innerHTML = `<i class="fa-solid fa-copy"></i>`);
