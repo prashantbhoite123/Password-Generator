80ab2d50a49747af3bdccd092495d2131900ea72
prashantbhoite123 <pbhoite985@gmail.com>
2024-05-01 16:46:39 +0530

    project logic change

--- script.js
+++ script.js
@@ -1,71 +1,46 @@
 const userVal = document.querySelector(".userVal");
 const generteCol = document.querySelector("button");
-const allinput = document.querySelectorAll("input");
-const copyBtn = document.querySelector(".copyBtn");
-const newInputs = Array.from(allinput);
+const symbolCheck = document.querySelector("#SymbolCheck");
+const numberCheck = document.querySelector("#numberCheck");
+const copyBtn = document.querySelector(".fa-copy");
+const rangeLabel = document.querySelector("#rangeLabel");
 
-let speech = new SpeechSynthesisUtterance();
-let key = "";
-newInputs.forEach((input) => {
-  input.addEventListener("click", (inval) => {
-    genraePassword(inval);
-  });
-});
-
-function genraePassword(e) {
-  e.target.innerHTML = "Change Password";
-  const sym = `a%bc!d!e*fh%34i%34$j%3434k*lm*n$#`;
-  const str = `abcdefg563hij3kl45mno35pqrstuvwxyz`;
-  lowerString = "";
-  SymbolString = "";
-  for (let i = 0; i < 8; i++) {
-    const lowerran = Math.floor(Math.random() * 8 + 1);
-    const symbol = Math.floor(Math.random() * 8 + 1);
-    lowerString += str[lowerran];
-    SymbolString += sym[symbol];
+function genraePassword() {
+  const range = document.querySelector("#range");
+  rangeLabel.innerHTML = `Length ${range.value}`;
+  const length = range.value;
+  let pass = "";
+  let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbersAllowed = numberCheck.checked;
+  const symbolAllowed = symbolCheck.checked;
+  if (numbersAllowed) str += "1234567890";
+  if (symbolAllowed) str += "!@#$%^&*";
+  for (let i = 0; i < length; i++) {
+    let char = Math.floor(Math.random() * str.length + 1);
+    pass += str.charAt(char);
   }
-  capital = lowerString.toUpperCase();
-  key = e.target.id;
+  userVal.innerHTML = pass;
 }
 
 generteCol.addEventListener("click", () => {
-  // console.log(userVal.innerHTM);
-  if (key == "numberChe") {
-    userVal.innerHTML = `${Math.floor(Math.random() * 99999999 + 10000000)}`;
-    speech.text = "number password is generated";
-    // console.log(userVal.innerHTML);
-  } else if (key == "lowerChe") {
-    userVal.innerHTML = `${lowerString}`;
-    speech.text = "lowercase password is generated";
-    // console.log(userVal.innerHTML);
-  } else if (key == "UpperChe") {
-    userVal.innerHTML = `${capital}`;
-    speech.text = "upperCase password is generated";
-    // console.log(userVal.innerHTML);
-  } else {
-    userVal.innerHTML = ` ${SymbolString}`;
-    speech.text = "symbol password is generated";
-  }
-  window.speechSynthesis.speak(speech);
-
-  console.log(userVal.innerHTML);
-  copyBtn.addEventListener("click", (e) => {
     const speech = new SpeechSynthesisUtterance();
-    speech.text = " your password is copyed";
-    window.speechSynthesis.speak(speech);
-    copyBtn.innerHTML === `<i class="fa-solid fa-copy"></i>`
-      ? (copyBtn.innerHTML = `<i class="fa-regular fa-copy"></i>`)
-      : (copyBtn.innerHTML = `<i class="fa-solid fa-copy"></i>`);
-    navigator.clipboard.writeText(userVal.innerHTML);
-  });
+    speech.text = "your password Was Generated";
+    speechSynthesis.speak(speech);
+  console.log();
+  genraePassword();
+  copyBtn.addEventListener(
+    "click",
+    (e) => {
+      const speech = new SpeechSynthesisUtterance();
+      speech.text = "your password is copied";
+      speechSynthesis.speak(speech);
+      console.log(e.target.classList);
+      e.target.classList.remove("fa-regular");
+      e.target.classList.add("fa-solid");
+      navigator.clipboard.writeText(userVal.innerHTML);
+    },
+    { once: true }
+  );
+  copyBtn.classList.remove("fa-solid");
+  copyBtn.classList.add("fa-regular");
 });
-
-// copyBtn.addEventListener("click", (e) => {
-//   const speech = new SpeechSynthesisUtterance();
-//   speech.text = " your password is copyed";
-//   window.speechSynthesis.speak(speech);
-//   copyBtn.innerHTML === `<i class="fa-solid fa-copy"></i>`
-//     ? (copyBtn.innerHTML = `<i class="fa-regular fa-copy"></i>`)
-//     : (copyBtn.innerHTML = `<i class="fa-solid fa-copy"></i>`);
-// navigator.clipboard.writeText(color);
-// });
